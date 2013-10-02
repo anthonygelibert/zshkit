a2517e2b1ab25988c76868daec7aaa8eb0194366
Anthony Gelibert <anthony.gelibert@me.com>
2013-10-02 14:01:12 +0200

    Add YAGARTO tools.

--- 01_path.zsh
+++ 01_path.zsh
@@ -34,6 +34,8 @@ export GIT_SCRIPTS="$PROJECTS_DIR/git-scripts:$PROJECTS_DIR/git-pull-request"
 export LIBEXEC="/usr/libexec"
 # VMware tools
 export VMWARE_TOOLS="/Applications/VMware Fusion.app/Contents/Library"
+# Yagarto
+export YAGARTO="/opt/yagarto/yagarto-4.7.2"
 
 # Some usefull paths (cd ~D)
 # My Documents
@@ -52,8 +54,8 @@ export Z="$PROJECTS_DIR/my_zshkit"
 typeset -U path cdpath fpath manpath
 
 # PATH
-export PATH="$VMWARE_TOOLS:$LIBEXEC:$GIT_SCRIPTS:$LATEX_BIN_DIR:$ALT_X11_DIR/bin:$MACPORTS_DIR/bin:$MACPORTS_DIR/sbin:$MANUALLY_INSTALLED_DIR/bin:$MANUALLY_INSTALLED_DIR/sbin:$APPLE_DEVELOPER_DIR/bin:$APPLE_DEVELOPER_DIR/sbin:$MY_APPLICATIONS_DIR:$DEVELOPER_TOOLS_COMMAND_LINE:/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:$X11_DIR/bin:."
-export MANPATH="$ALT_X11_DIR/share/man:$MACPORTS_DIR/share/man:$MANUALLY_INSTALLED_DIR/man:$MANUALLY_INSTALLED_DIR/share/man:$X11_DIR/share/man:$APPLE_DEVELOPER_DIR/share/man:/usr/share/man:/opt/local/etc/zsh/man/"
+export PATH="$YAGARTO/bin:$VMWARE_TOOLS:$LIBEXEC:$GIT_SCRIPTS:$LATEX_BIN_DIR:$ALT_X11_DIR/bin:$MACPORTS_DIR/bin:$MACPORTS_DIR/sbin:$MANUALLY_INSTALLED_DIR/bin:$MANUALLY_INSTALLED_DIR/sbin:$APPLE_DEVELOPER_DIR/bin:$APPLE_DEVELOPER_DIR/sbin:$MY_APPLICATIONS_DIR:$DEVELOPER_TOOLS_COMMAND_LINE:/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:$X11_DIR/bin:."
+export MANPATH="$JAVA_HOME/man:$YAGARTO/share/man:$ALT_X11_DIR/share/man:$MACPORTS_DIR/share/man:$MANUALLY_INSTALLED_DIR/man:$MANUALLY_INSTALLED_DIR/share/man:$X11_DIR/share/man:$APPLE_DEVELOPER_DIR/share/man:/usr/share/man:/opt/local/etc/zsh/man/"
 export FPATH="$HOME/.zsh/func:$FPATH:/opt/local/etc/zsh/zshrc.d/local-functions"
 export CDPATH=".:$HOME:$P:$C:$D:$DL:$MY_APPLICATIONS_DIR:$VOLUMES_DIR"
 
