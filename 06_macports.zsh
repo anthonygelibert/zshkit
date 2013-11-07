8a449e1764ce05ac88d32cac509e257733ebb279
Anthony Gelibert <anthony.gelibert@me.com>
2013-11-07 14:39:46 +0100

    Add alias for "ports variants"

--- 06_macports.zsh
+++ 06_macports.zsh
@@ -5,6 +5,7 @@ alias pi="sudo port -v install"
 alias ph="port search"
 alias po="port info"
 alias pc="port contents"
+alias pva="port variants"
 
 # Update the list, display the outdated, make the update and uninstall the inactives
 alias upMP='sudo port -v selfupdate && port outdated && sudo port -p upgrade outdated && sudo port -u -p uninstall'
