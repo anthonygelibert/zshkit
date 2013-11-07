7fab7eb1d3d47e350ef6327f2d62a4da83d517c6
Anthony Gelibert <anthony.gelibert@me.com>
2013-11-07 14:39:28 +0100

    Update Yagarto and JAVA_HOME.

--- 01_path.zsh
+++ 01_path.zsh
@@ -21,7 +21,7 @@ export X11_DIR="/usr/X11"
 # Mounted volumes
 export VOLUMES_DIR="/Volumes"
 # Java 1.7 Home on Mac OS X
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home"
 # Swap folder on Mac OS X
 export SWAP_DIR="/var/vm/"
 # Command-line tools from Developer Tools
@@ -35,7 +35,8 @@ export LIBEXEC="/usr/libexec"
 # VMware tools
 export VMWARE_TOOLS="/Applications/VMware Fusion.app/Contents/Library"
 # Yagarto
-export YAGARTO="/opt/yagarto/yagarto-4.7.2"
+export YAGARTO_NONE="/opt/yagarto/yagarto-4.7.2"
+export YAGARTO_ELF="/opt/yagarto/yagarto-4.4.2"
 
 # Some usefull paths (cd ~D)
 # My Documents
@@ -54,8 +55,8 @@ export Z="$PROJECTS_DIR/my_zshkit"
 typeset -U path cdpath fpath manpath
 
 # PATH
-export PATH="$YAGARTO/bin:$VMWARE_TOOLS:$LIBEXEC:$GIT_SCRIPTS:$LATEX_BIN_DIR:$ALT_X11_DIR/bin:$MACPORTS_DIR/bin:$MACPORTS_DIR/sbin:$MANUALLY_INSTALLED_DIR/bin:$MANUALLY_INSTALLED_DIR/sbin:$APPLE_DEVELOPER_DIR/bin:$APPLE_DEVELOPER_DIR/sbin:$MY_APPLICATIONS_DIR:$DEVELOPER_TOOLS_COMMAND_LINE:/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:$X11_DIR/bin:."
-export MANPATH="$JAVA_HOME/man:$YAGARTO/share/man:$ALT_X11_DIR/share/man:$MACPORTS_DIR/share/man:$MANUALLY_INSTALLED_DIR/man:$MANUALLY_INSTALLED_DIR/share/man:$X11_DIR/share/man:$APPLE_DEVELOPER_DIR/share/man:/usr/share/man:/opt/local/etc/zsh/man/"
+export PATH="$YAGARTO_ELF/bin:$YAGARTO_NONE/bin:$VMWARE_TOOLS:$LIBEXEC:$GIT_SCRIPTS:$LATEX_BIN_DIR:$ALT_X11_DIR/bin:$MACPORTS_DIR/bin:$MACPORTS_DIR/sbin:$MANUALLY_INSTALLED_DIR/bin:$MANUALLY_INSTALLED_DIR/sbin:$APPLE_DEVELOPER_DIR/bin:$APPLE_DEVELOPER_DIR/sbin:$MY_APPLICATIONS_DIR:$DEVELOPER_TOOLS_COMMAND_LINE:/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:$X11_DIR/bin:."
+export MANPATH="$JAVA_HOME/man:$YAGARTO_ELF/share/man:$YAGARTO_NONE/share/man:$ALT_X11_DIR/share/man:$MACPORTS_DIR/share/man:$MANUALLY_INSTALLED_DIR/man:$MANUALLY_INSTALLED_DIR/share/man:$X11_DIR/share/man:$APPLE_DEVELOPER_DIR/share/man:/usr/share/man:/opt/local/etc/zsh/man/"
 export FPATH="$HOME/.zsh/func:$FPATH:/opt/local/etc/zsh/zshrc.d/local-functions"
 export CDPATH=".:$HOME:$P:$C:$D:$DL:$MY_APPLICATIONS_DIR:$VOLUMES_DIR"
 
