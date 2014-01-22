d053dca95b4e01e8824cb69153f77394c6489f31
Anthony Gelibert <anthony.gelibert@me.com>
2014-01-22 15:51:02 +0100

    Enable egrep by default.

--- 06_exports.zsh
+++ 06_exports.zsh
@@ -3,7 +3,7 @@
 export EDITOR="mvim -f --nomru"
 export GEM_PATH="/Users/tony/.gem/ruby/1.9.1:/opt/local/lib/ruby1.9/gems/1.9.1:/opt/local/lib/ruby1.9/gems/"
 export GIT_EDITOR="mvim -f --nomru"
-export GREP_OPTIONS='--color=auto'
+export GREP_OPTIONS='-E --color=auto'
 export PAGER="most"
 export UNISONLOCALHOSTNAME="LAPTOP-MacBook_Pro"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share:/opt/local/share"
