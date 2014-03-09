c216a907cb3f0925eb6b55f10ec95f48a6241ac2
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-09 17:34:59 +0100

    Update all the pathes.

--- 01_path.zsh
+++ 01_path.zsh
@@ -18,8 +18,6 @@ export SANDBOX_DIR="${HOME}/Documents/SandBox"
 export VOLUMES_DIR="/Volumes"
 # Java 1.7 Home on Mac OS X
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home"
-# Swap folder on Mac OS X
-export SWAP_DIR="/var/vm/"
 # My help folder
 export HELPDIR="${HOME}/.zsh/help"
 # Git Scripts
@@ -28,9 +26,6 @@ export GIT_SCRIPTS="${PROJECTS_DIR}/git-scripts:${PROJECTS_DIR}/git-pull-request
 export LIBEXEC="/usr/libexec"
 # VMware tools
 export VMWARE_TOOLS="/Applications/VMware Fusion.app/Contents/Library"
-# Yagarto
-export YAGARTO_NONE="/opt/yagarto/yagarto-4.7.2"
-export YAGARTO_ELF="/opt/yagarto/yagarto-4.4.2"
 # MacPorts Coreutils
 export MP_COREUTILS="${MACPORTS_DIR}/libexec/gnubin/"
 
@@ -50,8 +45,8 @@ export Z="${PROJECTS_DIR}/my_zshkit"
 typeset -U path cdpath fpath manpath
 
 # PATH
-export PATH="${MP_COREUTILS}:${YAGARTO_ELF}/bin:${YAGARTO_NONE}/bin:${VMWARE_TOOLS}:${LIBEXEC}:${GIT_SCRIPTS}:${LATEX_BIN_DIR}:${ALT_X11_DIR}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MANUALLY_INSTALLED_DIR}/bin:${MY_APPLICATIONS_DIR}:${DEVELOPER_TOOLS_COMMAND_LINE}:/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:."
-export MANPATH="${MP_COREUTILS}/man:${JAVA_HOME}/man:${YAGARTO_ELF}/share/man:${YAGARTO_NONE}/share/man:${ALT_X11_DIR}/share/man:${MACPORTS_DIR}/share/man:${MANUALLY_INSTALLED_DIR}/man:${MANUALLY_INSTALLED_DIR}/share/man:/usr/share/man:/opt/local/etc/zsh/man/"
+export PATH="${MP_COREUTILS}:${VMWARE_TOOLS}:${LIBEXEC}:${GIT_SCRIPTS}:${LATEX_BIN_DIR}:${ALT_X11_DIR}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MANUALLY_INSTALLED_DIR}/bin:${MY_APPLICATIONS_DIR}:/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:."
+export MANPATH="${MP_COREUTILS}/man:${JAVA_HOME}/man:${ALT_X11_DIR}/share/man:${MACPORTS_DIR}/share/man:${MANUALLY_INSTALLED_DIR}/man:${MANUALLY_INSTALLED_DIR}/share/man:/usr/share/man:/opt/local/etc/zsh/man/"
 export FPATH="${HOME}/.zsh/func:${FPATH}:/opt/local/etc/zsh/zshrc.d/local-functions"
 export CDPATH=".:${HOME}:${P}:${C}:${D}:${VOLUMES_DIR}"
 
