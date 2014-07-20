653886620d8dd0ec370f4c9208608554c567efc0
Anthony Gelibert <anthony.gelibert@me.com>
2014-07-20 18:57:53 +0200

    Remove "/etc/hosts" file processing for completion.

--- 07_completion.zsh
+++ 07_completion.zsh
@@ -91,7 +91,7 @@ zstyle ':completion:*:javac:*' file-patterns '*.java'
 # djvu2pdf
 zstyle ':completion:*:djvu2pdf:*' file-patterns '*.djvu'
 
-# wine 
+# wine
 zstyle ':completion:*:wine:*' file-patterns '*.exe'
 
 # lua/luajit
@@ -159,11 +159,9 @@ zstyle ':completion:*:cd:*' ignored-patterns '(*/)#CVS'
 # use /etc/hosts and known_hosts for hostname completion
 [ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
 [ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
-[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
 hosts=(
   "$_global_ssh_hosts[@]"
   "$_ssh_hosts[@]"
-  "$_etc_hosts[@]"
   "$HOST"
   localhost
 )
