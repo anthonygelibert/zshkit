e02c6ca92d4ee479731342fd45665ad2ca4ec573
Anthony Gelibert <anthony.gelibert@me.com>
2014-09-16 23:07:55 +0200

    Add youtube-dl macros.

--- 06_youtube.zsh
+++ 06_youtube.zsh
@@ -1,3 +1,4 @@
 # vim: ft=zsh
 
-alias youtube-dl="youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --recode-video mp4 --all-subs -o '%(title)s.%(ext)s'"
+alias  ydl="youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --recode-video mp4 --all-subs --output '%(title)s.%(ext)s'"
+alias ydls="youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format mp3 --extract-audio --output '%(title)s.%(ext)s'"
