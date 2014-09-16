ee8b0c279b173a384fc8ad420fe812f32e378a6f
Anthony Gelibert <anthony.gelibert@me.com>
2014-09-16 23:06:00 +0200

    Add sudo macros.

---
+++ 06_sudo.zsh
@@ -0,0 +1,13 @@
+# vim: ft=zsh
+
+alias _='sudo '
+alias please='sudo'
+
+function sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
+    zle end-of-line
+}
+zle -N sudo-command-line
+# Defined shortcut keys: [Esc] [Esc]
+bindkey "\e\e" sudo-command-line
