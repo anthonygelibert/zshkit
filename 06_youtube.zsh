e3be36bdb59d68a0f068eaaa536fb5988f74cd88
Anthony Gelibert <anthony.gelibert@me.com>
2014-10-23 11:58:47 +0200

    ydls use best audio format rather than mp3.

--- 06_youtube.zsh
+++ 06_youtube.zsh
@@ -1,4 +1,4 @@
 # vim: ft=zsh
 
 alias  ydl="youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --recode-video mp4 --all-subs --output '%(title)s.%(ext)s'"
-alias ydls="youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format mp3 --extract-audio --output '%(title)s.%(ext)s'"
+alias ydls="youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --extract-audio --output '%(title)s.%(ext)s'"
