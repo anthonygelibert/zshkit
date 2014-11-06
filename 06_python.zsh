98baedcac8f2572d91d9d3b009ba89ff40d514f7
Anthony Gelibert <anthony.gelibert@me.com>
2014-11-06 17:30:55 +0100

    Correct WORKON_HOME folder.

--- 06_python.zsh
+++ 06_python.zsh
@@ -1,5 +1,5 @@
 # vim: ft=zsh
 
 export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
-export WORKON_HOME="${P}/python/"
+export WORKON_HOME="${P}/pyvenvs/"
 export IPYTHONDIR="${XDG_CONFIG_HOME}/python/ipython"
