c3ced7c2f2d977bd6e682b2ddc72bb81727dcb35
Anthony Gelibert <anthony.gelibert@me.com>
2014-11-10 14:19:19 +0100

    Aggressive cleaning after each update.

--- 12_alias.zsh
+++ 12_alias.zsh
@@ -41,7 +41,7 @@ function upProj() {
     for i in */; do
         (
             cd $i
-            [ -d .git ] && git up && exit 0
+            [ -d .git ] && git up && git agc && exit 0
             [ -d .hg  ] && hg pull && hg up && exit 0
             [ -d .svn ] && svn update && exit 0
             echo "\x1b[31mNo VCS used...\x1b[0m"
