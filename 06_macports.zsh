e0063e7f9e3aac5ce0389b625be2217e9a62e70a
Anthony Gelibert <anthony.gelibert@me.com>
2015-02-26 17:37:13 +0100

    MacPorts memento

--- 06_macports.zsh
+++ 06_macports.zsh
@@ -5,6 +5,7 @@ if [[ $(command -v port) ]]; then
     alias pi="sudo port -v install"
     # Don't remember the ph, po, pc and pva calls
     alias ph=" port search --name --line"
+# ph mm | awk -F $'\t' '{printf("%-44s %-12s %-s\n", $1,  $2,  $4)}'
     alias po=" port info"
     alias pc=" port contents"
     alias pva=" port variants"
