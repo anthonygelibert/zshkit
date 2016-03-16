67eb1152101110ab67496540d1a305a9f32a9771
Anthony Gelibert <anthony.gelibert@me.com>
2016-03-17 00:26:16 +0100

    Back to sublimetext

--- 06_editor.zsh
+++ 06_editor.zsh
@@ -1,5 +1,6 @@
 # vim: ft=zsh
 
-export EDITOR="atom-beta --wait"
-export GIT_EDITOR="atom-beta --wait"
-
+#export EDITOR="atom-beta --wait"
+export EDITOR="subl -n -w"
+#export GIT_EDITOR="atom-beta --wait"
+export GIT_EDITOR="subl -n -w"
