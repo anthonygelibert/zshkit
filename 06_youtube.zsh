2fffbd99c2bbb93828e0986c925e0d98cc4b4d6f
Anthony Gelibert <anthony.gelibert@me.com>
2016-05-31 20:51:11 +0200

    Update ydls options

--- 06_youtube.zsh
+++ 06_youtube.zsh
@@ -2,5 +2,5 @@
 
 if [[ $(command -v youtube-dl) ]]; then
     alias  ydl="noglob youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --recode-video mp4 --all-subs --output '%(title)s.%(ext)s'"
-    alias ydls="noglob youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --extract-audio --output '%(title)s.%(ext)s'"
+    alias ydls="noglob youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format m4a --extract-audio --output '%(title)s.%(ext)s'"
 fi
