08ae7c00e260ac8e21506a6cb5bc9777327ef5f5
Anthony Gelibert <anthony.gelibert@me.com>
2017-04-20 15:13:18 +0200

    Add CUDA_HOME

--- 01_path.zsh
+++ 01_path.zsh
@@ -27,6 +27,9 @@ export VMWARE_TOOLS="/Applications/VMware Fusion.app/Contents/Library"
 export MP_COREUTILS="${MACPORTS_DIR}/libexec/gnubin"
 # XDG
 export XDG_CONFIG_HOME="${HOME}/.config"
+# CUDA
+export CUDA_VERSION="8.0"
+export CUDA_HOME="/Developer/NVIDIA/CUDA-${CUDA_VERSION}"
 
 # Some usefull paths (cd ~D)
 # My Documents
