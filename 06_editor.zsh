7073659f8efb1ec76f2483710650d998235b69b6
Anthony Gelibert <anthony.gelibert@mac.com>
2018-07-27 01:31:57 +0200

    Use VS Code Insiders

--- 06_editor.zsh
+++ 06_editor.zsh
@@ -1,6 +1,8 @@
 # vim: ft=zsh
 
 #export EDITOR="atom-beta --wait"
-export EDITOR="subl -n -w"
+#export EDITOR="subl -n -w"
+export EDITOR="code-insiders -n -w"
 #export GIT_EDITOR="atom-beta --wait"
-export GIT_EDITOR="subl -n -w"
+#export GIT_EDITOR="subl -n -w"
+export GIT_EDITOR="code-insiders -n -w"
