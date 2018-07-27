a0abad586a31369498199bfc71c9b4f13ff59f95
Anthony Gelibert <anthony.gelibert@me.com>
2018-07-27 02:03:46 +0200

    Update youtube-dl default settings

--- 06_youtube.zsh
+++ 06_youtube.zsh
@@ -1,6 +1,6 @@
 # vim: ft=zsh
 
 if [[ $(command -v youtube-dl) ]]; then
-    alias  ydl="noglob youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format best --recode-video mp4 --all-subs --output '%(title)s.%(ext)s'"
-    alias ydls="noglob youtube-dl --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format m4a --extract-audio --output '%(title)s.%(ext)s'"
+    alias  ydl="noglob youtube-dl --embed-thumbnail --no-call-home --ignore-errors --add-metadata --xattrs --format best --audio-quality 0 --audio-format best --merge-output-format mp4 --recode-video mp4 --all-subs --fixup detect_or_warn --output '%(title)s.%(ext)s'"
+    alias ydls="noglob youtube-dl --embed-thumbnail --no-call-home --ignore-errors --add-metadata --xattrs --audio-quality 0 --audio-format m4a --extract-audio --fixup detect_or_warn --output '%(title)s.%(ext)s'"
 fi
