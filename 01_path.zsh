df4c3fc7ed9d334567f4a3949cf643efea2ad7a9
Anthony Gelibert <anthony.gelibert@me.com>
2018-08-03 14:48:32 +0200

    Update JDK

--- 01_path.zsh
+++ 01_path.zsh
@@ -17,9 +17,7 @@ export SANDBOX_DIR="${HOME}/Documents/BacASable"
 # Mounted volumes
 export VOLUMES_DIR="/Volumes"
 # Java 1.8 Home on Mac OS X
-#export JAVA_VERSION="1.8.0_144"
-#export JAVA_VERSION="9.0.4"
-export JAVA_VERSION="10.0.1"
+export JAVA_VERSION="10.0.2"
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-${JAVA_VERSION}.jdk/Contents/Home"
 # Git Scripts
 export GIT_SCRIPTS="${MY_APPLICATIONS_DIR}/git-scripts:${MY_APPLICATIONS_DIR}/git-pull-request:${MY_APPLICATIONS_DIR}/git-extra-commands/bin"
