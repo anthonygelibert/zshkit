c3aa84382eaca2db1f683e5760a229a5f740c96b
Anthony Gelibert <anthony.gelibert@me.com>
2018-08-17 21:38:44 +0200

    Add new options

--- 11_options.zsh
+++ 11_options.zsh
@@ -6,7 +6,7 @@
 
 setopt   ALIASES                # Expand aliases.
 unsetopt ALL_EXPORT             # NO -> All parameters subsequently defined are automatically exported.
-setopt   ALWAYS_TO_END          # If a completion with the cursor in the word was started and it results in only one match, the cursor is placed at the end of the word.
+unsetopt ALWAYS_TO_END          # NO -> If a completion with the cursor in the word was started and it results in only one match, the cursor is placed at the end of the word.
 setopt   APPEND_HISTORY         # If this is set, zsh sessions will append their history list to the history file, rather than overwrite it.
 setopt   AUTO_CD                # If a command is issued that can't be executed as a normal command, and the command is the name of a directory, perform the cd command to that directory.
 setopt   AUTO_CONTINUE          # With this option set, stopped jobs that are removed from the job table with the disown builtin command are automatically sent a CONT signal to make them running.
@@ -30,10 +30,11 @@ setopt   CDABLE_VARS            # If the argument to a cd command (or an implied
 setopt   CHASE_DOTS             # When changing to a directory containing a path segment '..' which would otherwise be treated as canceling the previous segment in the path (in other words, 'foo/..' would be removed from the path, or if '..' is the first part of the path, the last part of $PWD would be deleted), instead resolve the path to the physical directory.
 unsetopt CHASE_LINKS            # NO -> Resolve symbolic links to their true values when changing directory.
 setopt   CHECK_JOBS             # Report the status of background and suspended jobs before exiting a shell with job control; a second attempt to exit the shell will succeed. NO_CHECK_JOBS is best used only in combination with NO_HUP, else such jobs will be killed automatically.
+setopt   CHECK_RUNNING_JOBS     # Check for both running and suspended jobs when CHECK_JOBS is enabled.
 setopt   CLOBBER                # Allows `>' redirection to truncate existing files, and `>>' to create files. Otherwise `>!' or `>|' must be used to truncate a file, and `>>!' or `>>|' to create a file.
 setopt   COMBINING_CHARS        # Assume that the terminal displays combining characters correctly.  The shell cannot determine this by itself, hence the option.  This is highly recommended for MacOS X where file names are stored with accented characters split into basic and combining characters.
 unsetopt COMPLETE_ALIASES       # NO -> Prevents aliases on the command line from being internally substituted before completion is attempted.
-setopt   COMPLETE_IN_WORD       # If unset, the cursor is moved to the end of the word if completion is started. Otherwise it stays where it is and completion is done from both ends.
+unsetopt COMPLETE_IN_WORD       # If unset, the cursor is moved to the end of the word if completion is started. Otherwise it stays where it is and completion is done from both ends.
 setopt   CORRECT                # Try to correct the spelling of commands.
 unsetopt CORRECT_ALL            # NO -> Try to correct the spelling of all arguments in a line.
 setopt   CSH_JUNKIE_HISTORY     # A history reference without an event specifier will always refer to the previous command.
@@ -57,19 +58,21 @@ setopt   HASH_LIST_ALL          # Whenever a command completion is attempted, ma
 setopt   HIST_ALLOW_CLOBBER     # Add '|' to output redirections in the history.
 unsetopt HIST_BEEP              # NO -> Beep when an attempt is made to access a history entry which isn't there.
 setopt   HIST_EXPIRE_DUPS_FIRST # If the internal history needs to be trimmed to add the current command line, setting this option will cause the oldest history event that has a duplicate to be lost before losing a unique event from the list.
-unsetopt HIST_FCNTL_LOCK        # NO -> When writing out the history file, by default zsh uses ad-hoc file locking to avoid known problems with locking on some operating systems.
+setopt   HIST_FCNTL_LOCK          # When writing out the history file, by default zsh uses ad-hoc file locking to avoid known problems with locking on some operating systems.
 setopt   HIST_FIND_NO_DUPS      # When searching for history entries in the line editor, do not display duplicates of a line previously found, even if the duplicates are not contiguous.
 setopt   HIST_IGNORE_ALL_DUPS   # If a new command line being added to the history list duplicates an older one, the older command is removed from the list (even if it is not the previous event).
 setopt   HIST_IGNORE_DUPS       # Do not enter command lines into the history list if they are duplicates of the previous event.
 setopt   HIST_IGNORE_SPACE      # Remove command lines from the history list when the first character on the line is a space, or when one of the expanded aliases contains a leading space.
+setopt   HIST_LEX_WORDS         # When this option is set, words read in from a history file are divided up in a similar fashion to normal shell command line handling.
 unsetopt HIST_NO_FUNCTIONS      # NO -> Remove function definitions from the history list.
-setopt   HIST_NO_STORE          # Remove the history (fc -l) command from the history list when invoked.
+unsetopt HIST_NO_STORE          # NO -> Remove the history (fc -l) command from the history list when invoked.
 setopt   HIST_REDUCE_BLANKS     # Remove superfluous blanks from each command line being added to the history list.
+setopt   HIST_SAVE_NO_DUPS      # When writing out the history file, older commands that duplicate newer ones are omitted.
 setopt   HIST_VERIFY            # Whenever the user enters a line with history expansion, don't execute the line directly; instead, perform history expansion and reload the line into the editing buffer.
 unsetopt HUP                    # NO -> Send the HUP signal to running jobs when the shell exits.
 unsetopt IGNORE_BRACES          # NO -> Do not perform brace expansion.
 setopt   IGNORE_EOF             # Do not exit on end-of-file. Require the use of exit or logout instead.
-setopt   INC_APPEND_HISTORY     # This options works like APPEND_HISTORY except that new history lines are added to the $HISTFILE incrementally (as soon as they are entered), rather than waiting until the shell exits.
+unsetopt INC_APPEND_HISTORY     # NO -> This options works like APPEND_HISTORY except that new history lines are added to the $HISTFILE incrementally (as soon as they are entered), rather than waiting until the shell exits.
 setopt   INTERACTIVE_COMMENTS   # Allow comments even in interactive shells.
 setopt   LIST_AMBIGUOUS         # This option works when AUTO_LIST or BASH_AUTO_LIST is also set.
 unsetopt LIST_BEEP              # NO -> Beep on an ambiguous completion.
@@ -80,19 +83,21 @@ setopt   LONG_LIST_JOBS         # List jobs in the long format by default.
 setopt   MAGIC_EQUAL_SUBST      # All unquoted arguments of the form identifier=expression appearing after the command name have file expansion (that is, where expression has a leading `~' or `=') performed on expression as if it were a parameter assignment. The argument is not otherwise treated specially: in other words, it is subsequently treated as a single word, not as an assignment.
 unsetopt MAIL_WARNING           # NO -> Print a warning message if a mail file has been accessed since the shell last checked.
 setopt   MARK_DIRS              # Append a trailing / to all directory names resulting from filename generation (globbing).
-unsetopt MENU_COMPLETE          # NO -> On an ambiguous completion, instead of listing possibilities or beeping, insert the first match immediately.
+setopt   MENU_COMPLETE          # On an ambiguous completion, instead of listing possibilities or beeping, insert the first match immediately.
 setopt   MONITOR                # Allow job control.
 setopt   MULTIOS                # Perform implicit tees or cats when multiple redirections are attempted
+unsetopt MULTI_FUNC_DEF         # NO -> Allow definitions of multiple functions at once in the form ‘fn1 fn2...()’; if the option is not set, this causes a parse error.
 setopt   NOMATCH                # If a pattern for filename generation has no matches, print an error, instead of leaving it unchanged in the argument list. This also applies to file expansion of an initial ~ or =.
 setopt   NO_HUP                 #
 setopt   NOTIFY                 # Report the status of background jobs immediately, rather than waiting until just before printing a prompt.
 unsetopt NULL_GLOB              # NO -> If a pattern for filename generation has no matches, delete the pattern from the argument list instead of reporting an error.
 setopt   NUMERIC_GLOB_SORT      # If numeric filenames are matched by a filename generation pattern, sort the filenames numerically rather than lexicographically.
+unsetopt OCTAL_ZEROES           # NO -> Interpret any integer constant beginning with a 0 as octal, per IEEE Std 1003.2-1992 (ISO 9945-2:1993). This is not enabled by default as it causes problems with parsing of, for example, date and time strings with leading zeroes.
 setopt   PATH_DIRS              # Perform a path search even on command names with slashes in them. Thus if `/usr/local/bin' is in the user's path, and he types `X11/xinit', the command `/usr/local/bin/X11/xinit' will be executed (assuming it exists). This applies to the . builtin as well as to command execution. Commands explicitly beginning with `./' or `../' are not subject to path search.
 setopt   POSIX_BUILTINS         # When this option is set the command builtin can be used to execute shell builtin commands. Parameter assignments specified before shell functions and special builtins are kept after the command completes unless the special builtin is prefixed with the command builtin. Special builtins are ., :, break, continue, declare, eval, exit, export, integer, local, readonly, return, set, shift, source, times, trap and unset.
 setopt   POSIX_CD               # Modifies the behaviour of cd, chdir and pushd commands to make them more compatible with the POSIX standard.
 setopt   POSIX_JOBS             # This option makes job control more compliant with the POSIX standard.
-unsetopt PROMPT_SUBST           # If set, parameter expansion, command substitution and arithmetic expansion are performed in prompts.
+unsetopt PROMPT_SUBST           # NO -> If set, parameter expansion, command substitution and arithmetic expansion are performed in prompts.
 unsetopt PRINT_EXIT_VALUE       # NO -> Print the exit value of programs with non-zero exit status.
 setopt   PUSHD_IGNORE_DUPS      # Don't push multiple copies of the same directory onto the directory stack.
 setopt   PUSHD_MINUS            # Exchanges the meanings of `+' and `-' when used with a number to specify a directory in the stack.
