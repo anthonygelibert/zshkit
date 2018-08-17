9da9653066ce70e3987c54f74c9aee75ea7890c1
Anthony Gelibert <anthony.gelibert@me.com>
2018-08-17 11:04:53 +0200

    Update ph alias

--- 06_macports.zsh
+++ 06_macports.zsh
@@ -1,11 +1,11 @@
 # vim: ft=zsh
 
+# Some MacPorts alias
 if [[ $(command -v port) ]]; then
-    # Some MacPorts alias
     alias pi="sudo port -v install"
-    # Don't remember the ph, po, pc and pva calls
-    alias ph=" port search --name --line"
-# ph mm | awk -F $'\t' '{printf("%-44s %-12s %-s\n", $1,  $2,  $4)}'
+    function ph() {
+        port search --name --line "$1" | gawk -F $'\t' '{printf("%-44s %-12s %-s\n", $1,  $2,  $4)}'
+    }
     alias po=" port info"
     alias pc=" port contents"
     alias pva=" port variants"
