9e35d9c32429933e3e252d5253200530636a7f01
Anthony Gelibert <anthony.gelibert@me.com>
2018-08-22 11:34:23 +0200

    Add python macro for dev/debug/mem_debug modes

--- 06_python.zsh
+++ 06_python.zsh
@@ -4,6 +4,26 @@ export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
 export WORKON_HOME="${P}/pyvenvs/"
 export IPYTHONDIR="${XDG_CONFIG_HOME}/python/ipython"
 
+function python_dev() {
+    export PYTHONUNBUFFERED="enable"
+    export PYTHONUTF8="1"
+    export PYTHONWARNINGS="default"
+    export PYTHONDEVMODE="enable"
+    export PYTHONASYNCIODEBUG="enable"
+    export PYTHONFAULTHANDLER="enable"
+}
+
+function python_debug() {
+    export PYTHONINSPECT="enable"
+    export PYTHONVERBOSE="enable"
+    export PYTHONPROFILEIMPORTTIME="enable"
+}
+
+function python_mem_debug() {
+    export PYTHONTRACEMALLOC="enable"
+    export PYTHONMALLOC="pymalloc_debug"
+}
+
 alias pylint-quick='pylint --reports=n --include-ids=y'
 compdef _pylint-quick pylint-quick='pylint --reports=n --include-ids=y'
 alias serve_me='python -m http.server'
