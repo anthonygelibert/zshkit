b390ce661fd63ee02bbf51cdebed564abfbc7291
Anthony Gelibert <anthony.gelibert@me.com>
2019-02-11 16:42:54 +0100

    Don’t try to ignore dups to allow me to build stats on top of my shell usage

--- 11_options.zsh
+++ 11_options.zsh
@@ -58,16 +58,16 @@ setopt   HASH_LIST_ALL          # Whenever a command completion is attempted, ma
 setopt   HIST_ALLOW_CLOBBER     # Add '|' to output redirections in the history.
 unsetopt HIST_BEEP              # NO -> Beep when an attempt is made to access a history entry which isn't there.
 setopt   HIST_EXPIRE_DUPS_FIRST # If the internal history needs to be trimmed to add the current command line, setting this option will cause the oldest history event that has a duplicate to be lost before losing a unique event from the list.
-setopt   HIST_FCNTL_LOCK          # When writing out the history file, by default zsh uses ad-hoc file locking to avoid known problems with locking on some operating systems.
+setopt   HIST_FCNTL_LOCK        # When writing out the history file, by default zsh uses ad-hoc file locking to avoid known problems with locking on some operating systems.
 setopt   HIST_FIND_NO_DUPS      # When searching for history entries in the line editor, do not display duplicates of a line previously found, even if the duplicates are not contiguous.
-setopt   HIST_IGNORE_ALL_DUPS   # If a new command line being added to the history list duplicates an older one, the older command is removed from the list (even if it is not the previous event).
-setopt   HIST_IGNORE_DUPS       # Do not enter command lines into the history list if they are duplicates of the previous event.
+unsetopt HIST_IGNORE_ALL_DUPS   # NO -> If a new command line being added to the history list duplicates an older one, the older command is removed from the list (even if it is not the previous event).
+unsetopt HIST_IGNORE_DUPS       # NO -> Do not enter command lines into the history list if they are duplicates of the previous event.
 setopt   HIST_IGNORE_SPACE      # Remove command lines from the history list when the first character on the line is a space, or when one of the expanded aliases contains a leading space.
 setopt   HIST_LEX_WORDS         # When this option is set, words read in from a history file are divided up in a similar fashion to normal shell command line handling.
 unsetopt HIST_NO_FUNCTIONS      # NO -> Remove function definitions from the history list.
 unsetopt HIST_NO_STORE          # NO -> Remove the history (fc -l) command from the history list when invoked.
 setopt   HIST_REDUCE_BLANKS     # Remove superfluous blanks from each command line being added to the history list.
-setopt   HIST_SAVE_NO_DUPS      # When writing out the history file, older commands that duplicate newer ones are omitted.
+unsetopt HIST_SAVE_NO_DUPS      # NO -> When writing out the history file, older commands that duplicate newer ones are omitted.
 setopt   HIST_VERIFY            # Whenever the user enters a line with history expansion, don't execute the line directly; instead, perform history expansion and reload the line into the editing buffer.
 unsetopt HUP                    # NO -> Send the HUP signal to running jobs when the shell exits.
 unsetopt IGNORE_BRACES          # NO -> Do not perform brace expansion.
