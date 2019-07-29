156d681883d168099056e72030783a6917ae9c2f
Anthony Gelibert <anthony.gelibert@me.com>
2019-07-29 18:46:32 +0200

    Update SSH completion

--- 05_completion.zsh
+++ 05_completion.zsh
@@ -5,8 +5,6 @@
 #        ^X? (control-x ?) to run complete_debug with trace output
 
 # Files to ignore during completion
-fignore=(.o .pyc \~ .bak .junk .DS_Store $fignore)
-
 autoload -Uz compinit && compinit
 zmodload zsh/complist
 
@@ -146,15 +144,15 @@ compdef code-insiders='code'
 #########
 
 # Use /etc/hosts, known_hosts and SSH config for hostname completion
-[ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
-[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
-[ -r ~/.ssh/config ] && _ssh_config_hosts=(${(s: :)${(ps:\t:)${(f)"$(<$HOME/.ssh/config|grep 'Host')"}#Host}#Hostname}) || _ssh_config_hosts=()
-hosts=(
-  "$_global_ssh_hosts[@]"
-  "$_ssh_hosts[@]"
-  "$_ssh_config_hosts[@]"
-  localhost
-)
+#[ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
+#[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
+#[ -r ~/.ssh/config ] && _ssh_config_hosts=(${(s: :)${(ps:\t:)${(f)"$(<$HOME/.ssh/config|grep 'Host')"}#Host}#Hostname}) || _ssh_config_hosts=()
+#hosts=(
+#  "$_global_ssh_hosts[@]"
+#  "$_ssh_hosts[@]"
+#  "$_ssh_config_hosts[@]"
+#  localhost
+#)
 zstyle ':completion:*:hosts' hosts $hosts
 
 #########
