84ac5d605d3d8bacaa4933870927ace6d956c831
Anthony Gelibert <anthony.gelibert@me.com>
2020-02-25 22:44:53 +0100

    Remove old settings

--- 06_editor.zsh
+++ 06_editor.zsh
@@ -1,8 +1,4 @@
 # vim: ft=zsh
 
-#export EDITOR="atom-beta --wait"
-#export EDITOR="subl -n -w"
 export EDITOR="code-insiders -n -w"
-#export GIT_EDITOR="atom-beta --wait"
-#export GIT_EDITOR="subl -n -w"
 export GIT_EDITOR="code-insiders -n -w"
