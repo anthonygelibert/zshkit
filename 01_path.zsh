f45be7d945fe0b18d8ddbbfeaa00dce53f3f4207
Anthony Gelibert <anthony.gelibert@me.com>
2020-02-25 22:47:38 +0100

    Update all pathes

--- 01_path.zsh
+++ 01_path.zsh
@@ -14,8 +14,8 @@ export THEIR_PROJECTS_DIR="${HOME}/Documents/Others"
 export SANDBOX_DIR="${HOME}/Documents/BacASable"
 # Mounted volumes
 export VOLUMES_DIR="/Volumes"
-# Java 1.12 Home on Mac OS X
-export JAVA_VERSION="12.0.2"
+# Java 1.13 Home on Mac OS X
+export JAVA_VERSION="13.0.1"
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-${JAVA_VERSION}.jdk/Contents/Home"
 # Git Scripts
 export GIT_SCRIPTS="${MY_APPLICATIONS_DIR}/git-scripts:${MY_APPLICATIONS_DIR}/git-pull-request"
@@ -26,29 +26,28 @@ export XDG_CONFIG_HOME="${HOME}/.config"
 
 # Some usefull paths (cd ~D)
 # My Documents
-export D="${HOME}/Documents/"
+export D="${HOME}/Documents"
 # Current works
-export EC="${D}/EnCours/"
+export EC="${D}/EnCours"
 # My Downloads
-export DL="${HOME}/Downloads/"
+export DL="${HOME}/Downloads"
 # My Projects
-export P="${PROJECTS_DIR}/"
+export P="${PROJECTS_DIR}"
 # Their projects
-export O="${THEIR_PROJECTS_DIR}/"
+export O="${THEIR_PROJECTS_DIR}"
 # My Apps shortcut
 export A="${MY_APPLICATIONS_DIR}"
 # This project
 export Z="${PROJECTS_DIR}/my_zshkit"
-# CarbonBee
+# Corporate
 export C="${D}/CarbonBee"
-export REDAC="${C}/Recherche/AQiT/Redaction/"
-export CODE="${C}/Recherche/AQiT/Code/"
+export CODE="${C}/Recherche/AQiT/Code"
+export ETUDE="${C}/Etude"
 export AQITMAP="${CODE}/aqit-map"
 
 typeset -U path cdpath fpath manpath
 
-export          PATH="${HOME}/.local/bin:${HOME}/.iterm2:${AQITMAP}:${MY_APPLICATIONS_DIR}/ofexport2/bin:${MP_COREUTILS}:${VMWARE_TOOLS}:${GIT_SCRIPTS}:${JAVA_HOME}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MY_APPLICATIONS_DIR}:${MANUALLY_INSTALLED_DIR}/bin:/usr/libexec:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/MacGPG2/bin:/Library/TeX/texbin"
-export INTERNAL_PATH="${HOME}/.local/bin:${HOME}/.iterm2:${AQITMAP}:${MY_APPLICATIONS_DIR}/ofexport2/bin:${MP_COREUTILS}:${VMWARE_TOOLS}:${GIT_SCRIPTS}:${JAVA_HOME}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MY_APPLICATIONS_DIR}:${MANUALLY_INSTALLED_DIR}/bin:/usr/libexec/"
+export PATH="${HOME}/.local/bin:${HOME}/.iterm2:${AQITMAP}:${MP_COREUTILS}:${GIT_SCRIPTS}:${JAVA_HOME}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MY_APPLICATIONS_DIR}:${MANUALLY_INSTALLED_DIR}/bin:/usr/libexec:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/MacGPG2/bin:/Library/TeX/texbin:."
 export MANPATH="${MP_COREUTILS}/man:${JAVA_HOME}/man:${MACPORTS_DIR}/share/man:${MANUALLY_INSTALLED_DIR}/man:${MANUALLY_INSTALLED_DIR}/share/man:${MACPORTS_DIR}/etc/zsh/man/:/usr/share/man"
-export FPATH="${FPATH}:${MACPORTS_DIR}/share/zsh/site-functions/:${MACPORTS_DIR}/etc/zsh/zshrc.d/local-functions/darwin:${MACPORTS_DIR}/etc/zsh/zshrc.d/local-functions/general:$Z/functions"
-export CDPATH=".:${HOME}:${P}:${D}:${C}:${REDAC}:${CODE}:${DL}:${O}:${EC}:${VOLUMES_DIR}"
+export FPATH="${FPATH}:${MACPORTS_DIR}/share/zsh/site-functions/:${MACPORTS_DIR}/etc/zsh/zshrc.d/local-functions/general:$Z/functions"
+export CDPATH=".:${HOME}:${P}:${D}:${C}:${CODE}:${ETUDE}"
