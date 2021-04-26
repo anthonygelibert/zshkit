3b777782c2be154f3cd390093f26c9f22f8f6793
Anthony Gelibert <anthony.gelibert@me.com>
2021-04-26 11:27:51 +0200

    Set PCLFONTSOURCE macro

---
+++ 06_ghostscript.zsh
@@ -0,0 +1,4 @@
+# GhostPCL requires a set of truetype fonts to function properly.
+# To use the default URW font set, add the following to your shell profile:
+
+export PCLFONTSOURCE='/opt/local/share/ghostscript/urwfonts/'
