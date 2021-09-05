3df856f6c5ce2e57e38796f9d0489b1dafd27fe0
Anthony Gelibert <anthony.gelibert@me.com>
2021-09-05 10:59:56 +0200

    Fix PATH

--- 01_path.zsh
+++ 01_path.zsh
@@ -18,7 +18,7 @@ export VOLUMES_DIR="/Volumes"
 export JAVA_VERSION="16.0.2"
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-${JAVA_VERSION}.jdk/Contents/Home"
 # Git Scripts
-export GIT_SCRIPTS="${MY_APPLICATIONS_DIR}/git-scripts:${MY_APPLICATIONS_DIR}/git-pull-request"
+export GIT_SCRIPTS="${MY_APPLICATIONS_DIR}/git-scripts:${MY_APPLICATIONS_DIR}/git-extra-commands/bin"
 export DIFF_SO_FANCY="${MY_APPLICATIONS_DIR}/diff-so-fancy"
 # MacPorts Coreutils
 export MP_COREUTILS="${MACPORTS_DIR}/libexec/gnubin"
@@ -43,7 +43,7 @@ export C="${D}/CarbonBee"
 
 typeset -U path cdpath fpath manpath
 
-export PATH="${HOME}/Library/Caches/com.github.rikvdkleij.intellij-haskell/lts-15/bin:${DIFF_SO_FANCY}:${HOME}/Library/Python/3.9/bin:${HOME}/Library/Python/3.8/bin:${HOME}/.local/bin:${HOME}/.iterm2:${MP_COREUTILS}:${GIT_SCRIPTS}:${JAVA_HOME}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MY_APPLICATIONS_DIR}:${MANUALLY_INSTALLED_DIR}/bin:/usr/libexec:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/MacGPG2/bin:/Library/TeX/texbin:."
+export PATH="${DIFF_SO_FANCY}:${HOME}/.iterm2:${MP_COREUTILS}:${GIT_SCRIPTS}:${JAVA_HOME}/bin:${MACPORTS_DIR}/bin:${MACPORTS_DIR}/sbin:${MY_APPLICATIONS_DIR}:${MANUALLY_INSTALLED_DIR}/bin:/usr/libexec:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/MacGPG2/bin:/Library/TeX/texbin:."
 export MANPATH="${MP_COREUTILS}/man:${JAVA_HOME}/man:${MACPORTS_DIR}/share/man:${MANUALLY_INSTALLED_DIR}/man:${MANUALLY_INSTALLED_DIR}/share/man:${MACPORTS_DIR}/etc/zsh/man/:/usr/share/man"
 export FPATH="${FPATH}:${MACPORTS_DIR}/share/zsh/site-functions/:${MACPORTS_DIR}/etc/zsh/zshrc.d/local-functions/general:${MACPORTS_DIR}/etc/zsh/zshrc.d/local-functions/opt:/opt/local/etc/zsh/zshrc.d/local-functions/darwin:/opt/local/share/git/contrib/completion:${Z}/functions"
 export CDPATH=".:${HOME}:${P}:${D}:${C}"
