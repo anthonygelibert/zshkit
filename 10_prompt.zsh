869f095275058873cefbfdbfe3a795a611be5022
Anthony Gelibert <anthony.gelibert@me.com>
2024-12-21 14:10:43 +0100

    🔧 Update prompt

--- 10_prompt.zsh
+++ 10_prompt.zsh
@@ -1,15 +1,10 @@
 # vim: ft=zsh
 
-export PS1='☕️ '
+export PS1=' ☕️ '
 # secondary prompt, printed when the shell need
 # more information to complete a command.
-PS2='%_> '
-
+export PS2='%_> '
 # selection prompt used within a select loop.
-PS3='?# '
-
+export PS3='?# '
 # the execution trace prompt (setopt xtrace). default: '+%N:%i>'
 export PS4='%B[%*]%b [%L] (%N:%i) %_ '
-
-# Right prompt with clock: I disable it, it's now a feature of iTerm
-# export RPS1="%{$fg_bold[black]%}%D{%d/%m/%y %H:%M:%S}%{${reset_color}%}"
