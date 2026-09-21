02f27a9eea986e6ffd8c3eeb3e6b3a7ca0ca4b65
Anthony Gelibert <anthony.gelibert@me.com>
2026-09-21 11:53:53 +0200

    🔥 Remove comments

--- 10_bindkey.zsh
+++ 10_bindkey.zsh
@@ -32,9 +32,7 @@ bindkey . rationalize-dot
 # ↑/↓ : history-beginning-search (par préfixe)
 bindkey '^[[A' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
-# Ctrl-R : recherche incrémentale inverse (enlevé pour fzf)
-#bindkey '^R' history-incremental-pattern-search-backward
-# Ctrl-S : (souvent bloqué par flow control ; vous avez NO_FLOW_CONTROL) recherche vers l'avant
+# Ctrl-S :  recherche vers l'avant
 bindkey '^S' history-incremental-pattern-search-forward
 
 # Terminal.app sucks :'(
